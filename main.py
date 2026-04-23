68473a1696cb6148a981db5c94cab233093088f3
vinesgonzalez <vivi7669@gmail>
2026-04-23 16:07:41 -0500

    Vivi_GC calculadora

---
+++ main.py
@@ -0,0 +1,5 @@
+numero1=float(input("Digite el primer número: "))
+numero2=float(input("Digite el segundo numero: "))
+suma=numero1+numero2
+# mostrar el resultado
+print("La suma de los números es:", suma)
